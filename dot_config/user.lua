c9ce220727144538a688dcb0bd8fb66e59b57764
Srinath10X <srinathcreates@gmail.com>
2024-10-31 18:18:31 +0530

    added user.lua of my litevim config

--- dot_config/user.lua
+++ dot_config/user.lua
@@ -1,48 +1,34 @@
 -- User plugins here
 
 local M = {
-	plugins = {
-		{ "mattn/emmet-vim", "szw/vim-maximizer" },
-		{
-			"uga-rosa/ccc.nvim",
-			event = { "BufReadPre", "BufNewFile" },
-			opts = {
-				highlighter = {
-					auto_enable = true,
-					lsp = true,
-				},
-			},
-		},
-		{
-			"iamcco/markdown-preview.nvim",
-			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-			-- If you have yarn installed just change the build command
-			build = "cd app && npm install",
-			lazy = false,
-			init = function()
-				vim.g.mkdp_filetypes = { "markdown" }
-			end,
-			ft = { "markdown" },
-		},
-		{
-			"Exafunction/codeium.vim",
-			config = function()
-				-- Change '<C-g>' here to any keycode you like.
-				vim.keymap.set("i", "<C-g>", function()
-					return vim.fn["codeium#Accept"]()
-				end, { expr = true, silent = true })
-				vim.keymap.set("i", "<c-;>", function()
-					return vim.fn["codeium#CycleCompletions"](1)
-				end, { expr = true, silent = true })
-				vim.keymap.set("i", "<c-,>", function()
-					return vim.fn["codeium#CycleCompletions"](-1)
-				end, { expr = true, silent = true })
-				vim.keymap.set("i", "<c-x>", function()
-					return vim.fn["codeium#Clear"]()
-				end, { expr = true, silent = true })
-			end,
-		},
+	markdown = {
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+
+	codium = {
+		"Exafunction/codeium.vim",
+		event = "VimEnter",
+		config = function()
+			vim.keymap.set("i", "<C-g>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true, silent = true })
+			vim.keymap.set("i", "<c-;>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true, silent = true })
+			vim.keymap.set("i", "<c-,>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true, silent = true })
+			vim.keymap.set("i", "<c-x>", function()
+				return vim.fn["codeium#Clear"]()
+			end, { expr = true, silent = true })
+		end,
 	},
 }
 
-return M.plugins
+return { M.markdown, M.codium }
