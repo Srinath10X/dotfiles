301f8c9f12043ce19b58fcef6290d023d863bdd9
Srinath10X <srinathcreates@gmail.com>
2024-12-20 02:18:19 +0530

    refactor: removed redundant nvim_packer moved to litevim repo

--- dot_config/nvim_packer/lua/core/colorscheme.lua
+++
@@ -1,6 +0,0 @@
-local success, _ = pcall(vim.cmd,"colorscheme catppuccin")
-
-if not success then
-	print("Colorscheme not found catppucccin")
-	return
-end 
--- dot_config/nvim_packer/lua/plugins/nvim-colorizer.lua
+++
@@ -1,6 +0,0 @@
-local colorizer_setup = pcall(require,"ColorizerAttachToBuffer")
-
-if not colorizer_setup then
-	print("colorizer not found")
-	return
-end
